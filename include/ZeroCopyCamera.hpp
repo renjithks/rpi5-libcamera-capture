e4b7914cfb60a740c29549eefc68a8d8536ee8ee
renjithks <renjith.rohin@gmail.com>
2025-07-15 07:37:07 +0900

    Added .h include for class

---
+++ include/ZeroCopyCamera.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <libcamera/libcamera.h>
+#include <libcamera/camera_manager.h>
+#include <libcamera/framebuffer_allocator.h>
+
+#include <memory>
+#include <vector>
+
+class ZeroCopyCamera {
+public:
+    bool initialize();
+    void shutdown();
+
+private:
+    void requestComplete(libcamera::Request *request);
+
+    std::unique_ptr<libcamera::CameraManager> manager_;
+    std::shared_ptr<libcamera::Camera> camera_;
+    std::unique_ptr<libcamera::CameraConfiguration> config_;
+    std::unique_ptr<libcamera::FrameBufferAllocator> allocator_;
+    std::vector<std::unique_ptr<libcamera::Request>> requests_;
+};
