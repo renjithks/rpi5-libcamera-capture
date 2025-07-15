3a131935d4b32691cbc5d8f743013302ccefa39e
renjithks <renjith.rohin@gmail.com>
2025-07-16 07:23:38 +0900

    Modified .hpp to add stream support

--- include/ZeroCopyCamera.hpp
+++ include/ZeroCopyCamera.hpp
@@ -1,9 +1,10 @@
-#pragma once
+#ifndef ZERO_COPY_CAMERA_HPP
+#define ZERO_COPY_CAMERA_HPP
 
 #include <libcamera/libcamera.h>
 #include <libcamera/camera_manager.h>
 #include <libcamera/framebuffer_allocator.h>
-
+#include <libcamera/request.h>
 #include <memory>
 #include <vector>
 
@@ -11,15 +12,18 @@ class ZeroCopyCamera {
 public:
     ZeroCopyCamera();
     ~ZeroCopyCamera();
-    bool initialize();
-    void shutdown();
+
+    void start();
 
 private:
-    void requestComplete(libcamera::Request *request);
+    void requestComplete(libcamera::Request* request);
 
     std::unique_ptr<libcamera::CameraManager> manager_;
     std::shared_ptr<libcamera::Camera> camera_;
     std::unique_ptr<libcamera::CameraConfiguration> config_;
+    libcamera::Stream* stream_;
     std::unique_ptr<libcamera::FrameBufferAllocator> allocator_;
     std::vector<std::unique_ptr<libcamera::Request>> requests_;
 };
+
+#endif // ZERO_COPY_CAMERA_HPP
